2a96a95227a15214e45ec7c0f19f24e742a7b113
Alex Poltorapavlov <poltorapavlov.iskander@gmail.com>
2024-04-22 08:39:01 +0300

    Update: it just works

--- parsing.rb
+++ parsing.rb
@@ -6,7 +6,7 @@ class Parsing
 
   INFO_TYPE = { countres: ["OKSMList", 13],
                 regions: ["OKATOList", 10],
-                docs: ["Kod_documentaList", 1]}.freeze
+                docs: ["Kod_documentaList", 0]}.freeze
 
   def initialize(path: 'data.csv')
     @data = CSV.read(path, headers: true)
@@ -26,17 +26,22 @@ class Parsing
     parse_data(:docs)
   end
 
+  def name_format(name, kind_of_info)
+    name_rus = name
+    start = INFO_TYPE[kind_of_info][1]
+    name_rus = name_rus.slice(start...-6)
+    name_rus =  Translit.convert(name_rus, :russian) if name_rus
+    name_rus = name_rus.gsub(/(?<=[^ ])([A-ZА-Я])/, ' \1').strip if name_rus
+  end
+
   def parse_data(kind_of_info)
     timestamp = Time.now.strftime("%Y%m%d%H%M%S")
     CSV.open("output/#{kind_of_info.to_s}_#{timestamp}.csv", "w", col_sep: ";") do |csv|
 
     @data.each do |row|
       if row['extended link role'].include?(INFO_TYPE[kind_of_info][0])
-        name_rus = row['name']
-        start = INFO_TYPE[kind_of_info][1]
-        name_rus = name_rus.slice(start...-6)
-        name_rus =  Translit.convert(name_rus, :russian) if name_rus
-        csv << [row['label'], name_rus, row['name']]
+        name = name_format(row['name'], kind_of_info)
+        csv << [row['label'], name, row['name']]
       end
     end
   end
