12075418a27a44adcf9a4e9cc5a9169901a4bea1
Alex Poltorapavlov <poltorapavlov.iskander@gmail.com>
2024-04-22 05:16:29 +0300

    Update: added initialize, data_read and data_write_methods for Parsing

--- parsing.rb
+++ parsing.rb
@@ -1,7 +1,23 @@
 require 'csv'
 
 class Parsing
-  def initialize
+
+  def initialize(path: 'data.csv')
+    @data = CSV.read(path)
+  end
+
+  def data_read
+    @data
+  end
+
+  def data_write(path)
+    @data = CSV.read(path)
+  end
+
+  def load_file
 
   end
 end
+
+a = Parsing.new
+p a.data
